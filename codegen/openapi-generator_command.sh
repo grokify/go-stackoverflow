00e1aebe9b3806f9b2de5bd30a45ca376bbe6196
John Wang <johncwang@gmail.com>
2020-09-23 02:33:48 -0700

    codegen: spec: update

--- codegen/openapi-generator_command.sh
+++ codegen/openapi-generator_command.sh
@@ -1,3 +1,8 @@
-java -jar openapi-generator-cli.jar generate -c openapi-generator_config.json -i openapi_spec.yaml -g go -o client
-echo "\n\nfunc (apiClient *APIClient) HTTPClient() *http.Client { return apiClient.cfg.HTTPClient }" >> client/client.go
-gofmt -s -w client/*.go
+#perl -p -i -e 's/\s+\[Beta\]\s*$/\n/g' openapi-spec.json
+export GO_POST_PROCESS_FILE="/usr/local/go/bin/gofmt -s -w"
+
+java -jar openapi-generator-cli.jar generate -i openapi_v3.0.json -g go -o stackoverflow --package-name stackoverflow
+
+#perl -p -i -e 's/(delimiter,\s+-1\),\s+"\[\]"\))/$1\n} else if t, ok := obj.(time.Time); ok {\nreturn t.Format(time.RFC3339)/g' client/client.go
+echo "\n\nfunc (apiClient *APIClient) HTTPClient() *http.Client { return apiClient.cfg.HTTPClient }" >> stackoverflow/client.go
+gofmt -s -w stackoverflow/*.go
