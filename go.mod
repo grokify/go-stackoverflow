07ec46df0b121deac44d4782fd00541021e33e36
John Wang <johncwang@gmail.com>
2022-06-08 20:32:49 -0700

    dep: go mod: update

--- go.mod
+++ go.mod
@@ -4,10 +4,9 @@ go 1.16
 
 require (
 	github.com/antihax/optional v1.0.0
-	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/grokify/elastirad-go v0.2.2
+	github.com/grokify/elastirad-go v0.4.0
 	github.com/grokify/gocharts/v2 v2.7.3
 	github.com/grokify/mogo v0.39.6
 	github.com/jessevdk/go-flags v1.5.0
-	golang.org/x/oauth2 v0.0.0-20220411215720-9780585627b5
+	golang.org/x/oauth2 v0.0.0-20220524215830-622c5d57e401
 )
--- util/elasticsearch/users.go
+++ util/elasticsearch/users.go
@@ -1,23 +1,23 @@
 package elasticsearch
 
-import v5 "github.com/grokify/elastirad-go/models/v5"
+import "github.com/grokify/elastirad-go/models/es5"
 
-func CreateUsersRequestBody(shardCount, replicaCount uint32, objectName string) v5.CreateIndexBody {
+func CreateUsersRequestBody(shardCount, replicaCount uint32, objectName string) es5.CreateIndexBody {
 	if shardCount == 0 {
 		shardCount = 1
 	}
 	if replicaCount == 0 {
 		replicaCount = 1
 	}
-	body := v5.CreateIndexBody{
-		Index: &v5.Index{
+	body := es5.CreateIndexBody{
+		Index: &es5.Index{
 			NumberOfShards:   shardCount,
 			NumberOfReplicas: replicaCount,
 		},
-		Mappings: map[string]v5.Mapping{
+		Mappings: map[string]es5.Mapping{
 			objectName: {
-				All: v5.All{Enabled: true},
-				Properties: map[string]v5.Property{
+				All: es5.All{Enabled: true},
+				Properties: map[string]es5.Property{
 					"about_me":      {Type: "text"},
 					"display_name":  {Type: "text"},
 					"link":          {Type: "text"},
